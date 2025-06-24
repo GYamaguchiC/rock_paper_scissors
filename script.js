267dca40c273027ffbc288f3b20d1d945efbab96
Gabriel Yamaguchi <106696472+GYamaguchiC@users.noreply.github.com>
2025-06-24 21:52:17 +0900

    add getHumanChoice and start scores at 0

---
+++ script.js
@@ -0,0 +1,8 @@
+function getHumanChoice() {
+    return prompt("Choose one of the following options to play:\n1.Rock\n2.Paper\n3.Scissors");
+}
+
+let humanScore = 0, computerScore = 0;
+console.log(humanScore, computerScore)
+
+getHumanChoice();
